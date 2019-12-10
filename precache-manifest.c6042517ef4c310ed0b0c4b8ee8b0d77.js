63949f4cfd898652f809cd09b4106a2e08f67e3d
Mansi <mansikamra00@gmail.com>
2019-12-10 13:53:08 +0530

    Updates

---
+++ precache-manifest.c6042517ef4c310ed0b0c4b8ee8b0d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127dfbd99157f32bc444a51c079abd7f",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "7eddee792f0a2b1a53f7",
+    "url": "/phone-directory/static/css/main.6f9dbee9.chunk.css"
+  },
+  {
+    "revision": "ad88fc23cbbb8365a20c",
+    "url": "/phone-directory/static/js/2.0e4944c0.chunk.js"
+  },
+  {
+    "revision": "7eddee792f0a2b1a53f7",
+    "url": "/phone-directory/static/js/main.1b727257.chunk.js"
+  },
+  {
+    "revision": "123152631e28e7abc5f0",
+    "url": "/phone-directory/static/js/runtime-main.337be516.js"
+  }
+]);
